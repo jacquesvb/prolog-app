af72aef714ce287735263dad7c5e6e98e786b2d4
Jacques Van Blokland <jacquesvb@live.com>
2023-12-03 19:32:47 -0800

    First commit

---
+++ pages/index.jsx
@@ -0,0 +1,37 @@
+import { Routes } from "@config/routes";
+import styles from "./index.module.scss";
+
+const IssuesPage = () => {
+  return (
+    <div>
+      <header className={styles.header}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <span className={styles.logo}>
+          <img src="/icons/logo-large.svg" alt="Prolog logo" />
+        </span>
+        <span className={styles.links}>
+          <a href={Routes.home}>Home</a>
+          <a href={Routes.products}>Products</a>
+          <a href={Routes.documentation}>Documentation</a>
+          <a href={Routes.pricing}>Pricing</a>
+        </span>
+        <span className={styles.dashboard}>
+          <a href={Routes.projects}>Open Dashboard</a>
+        </span>
+      </header>
+      <button
+        className={styles.contactButton}
+        onClick={() =>
+          alert(
+            "Implement this in Challenge 2 - Modal:\n\nhttps://profy.dev/rjs-challenge-modal",
+          )
+        }
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/icons/message.svg" alt="Contact" />
+      </button>
+    </div>
+  );
+};
+
+export default IssuesPage;
